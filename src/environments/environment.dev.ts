39d1af7cecdb5a7226c1e27e1b4306ad0d7bdd1a
Klee Collage <klee.collage@gmail.com>
2025-04-17 16:52:08 -0600

    feat(firebase): Implement config to connect with firebase

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: false,
-  name: 'dev'
+  name: 'dev',
+  firebase: {
+    config: {
+      apiKey: "AIzaSyC78dsjSX2bgc-XKkLg5DzR9vEF_y8xP-8",
+      authDomain: "estates-app-d314b.firebaseapp.com",
+      projectId: "estates-app-d314b",
+      storageBucket: "estates-app-d314b.firebasestorage.app",
+      messagingSenderId: "608569932241",
+      appId: "1:608569932241:web:1bf23d64a8b3efaf04c8f0"
+    }
+  }
 };
