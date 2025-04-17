19628609c22670826ab11fe5c49fc5e044446e86
Klee Collage <klee.collage@gmail.com>
2025-04-17 13:26:46 -0600

    feat(EstateProfile): Implement maps from estate to estateResponseDto and visceverse

---
+++ NetKubernetes/Profiles/EstateProfile.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using NetKubernetes.Dto.EstateDto;
+using NetKubernetes.Models;
+
+namespace NetKubernetes.Profiles;
+
+public class EstateProfile: Profile
+{
+    public EstateProfile()
+    {
+        CreateMap<Estate, EstateResponseDto>();
+        CreateMap<EstateResponseDto, Estate>();
+    }
+}
