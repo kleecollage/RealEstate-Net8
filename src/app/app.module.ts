f559a8089ae674fdc8a2e25ecb23731c4330fcaf
Klee Collage <klee.collage@gmail.com>
2025-04-18 15:21:42 -0600

    feat(components): Implement Header component with navigation menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,36 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
-
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { NotificationModule } from '@app/services';
 import { IndicatorsModule } from '@app/shared/indicators';
 import { environment } from '@src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PopupsModule } from './shared/popups';
-
-import { NotificationModule } from '@app/services';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuListComponent } from './components/menu-list/menu-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuListComponent
   ],
   imports: [
     BrowserModule,
@@ -38,7 +46,13 @@ import { NotificationModule } from '@app/services';
     IndicatorsModule,
     BrowserAnimationsModule,
     PopupsModule,
-    NotificationModule.forRoot()
+    NotificationModule.forRoot(),
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
