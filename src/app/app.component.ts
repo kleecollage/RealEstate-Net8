e1557564cdc6389e72620700682df62ce504162e
Klee Collage <klee.collage@gmail.com>
2025-04-17 18:03:19 -0600

    feat(firebase): Gettig data from firebase OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
-import { environment } from '@src/environments/environment';
-import { test } from '@app/test'
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'cli-inmueble-app';
+
+  constructor(private fs:AngularFirestore){}
+
+  ngOnInit(): void {
+    this.fs.collection('test').stateChanges().subscribe(people => {
+      console.log(people.map(person => person.payload.doc.data()));
+    })
+  }
+
+
+
 }
