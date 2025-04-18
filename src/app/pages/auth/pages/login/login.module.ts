d738b24db2054d6c0c43597876b3e0886bc98446
Klee Collage <klee.collage@gmail.com>
2025-04-18 16:15:43 -0600

    feat(pages): Implement login page

--- src/app/pages/auth/pages/login/login.module.ts
+++ src/app/pages/auth/pages/login/login.module.ts
@@ -1,5 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
@@ -11,7 +18,14 @@ import { LoginComponent } from './login.component';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatInputModule,
+    FormsModule,
+    FlexLayoutModule,
   ]
 })
 export class LoginModule { }
