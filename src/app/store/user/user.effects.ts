98f3ca17a8a64654e9af07bb9d58f6af0f96cd5c
Klee Collage <klee.collage@gmail.com>
2025-04-18 18:24:55 -0600

    feat(effects): Implement user effect to sign in and get user session

--- src/app/store/user/user.effects.ts
+++ src/app/store/user/user.effects.ts
@@ -5,7 +5,8 @@ import { NotificationService } from '@app/services';
 import { UserResponse } from '@app/store/user/user.models';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { environment } from '@src/environments/environment';
-import { catchError, map, Observable, of, switchMap, tap } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError, map, switchMap, tap } from 'rxjs/operators';
 import * as fromActions from './user.actions';
 
 type Action = fromActions.All;
@@ -37,6 +38,50 @@ export class UserEffects {
           })
         )
       )
+    )
+  )
+
+  signInEmail$: Observable<Action> = createEffect(() =>
+    this.actions.pipe(
+      ofType(fromActions.Types.SIGN_IN_EMAIL),
+      map((action: fromActions.SignInEmail) => action.credentials),
+      switchMap( userData =>
+        this.httpCient.post<UserResponse>(`${environment.url}user/login/`, userData)
+        .pipe(
+          tap((response: UserResponse) => {
+            localStorage.setItem('token', response.token);
+            this.router.navigate(['/']);
+          }),
+          map((response: UserResponse) => new fromActions.SignInEmailSuccess(response)),
+          catchError(err => {
+            this.notification.error("Credentials incorrect");
+            return of(new fromActions.SignInEmailError(err.message));
+          })
+        )
+      )
+    )
+  )
+
+  init$: Observable<Action> = createEffect(() =>
+    this.actions.pipe(
+      ofType(fromActions.Types.INIT),
+      switchMap( async () => localStorage.getItem('token')),
+      switchMap( token => {
+        if (token) {
+          return this.httpCient.get<UserResponse>(`${environment.url}user`)
+          .pipe(
+            tap((response: UserResponse) => {
+              console.log('user session data from server', response)
+            }),
+            map((response: UserResponse) => new fromActions.InitAuthorized(response.email, response || null)),
+            catchError(err => {
+              return of(new fromActions.InitError(err.message));
+            })
+          )
+        } else {
+          return of(new fromActions.InitUnauthorized());
+        }
+      })
     )
   )
 }
