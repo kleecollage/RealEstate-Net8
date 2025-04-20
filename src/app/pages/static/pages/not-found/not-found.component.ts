78f1013568bd04a5b27b499fcdcd4a0764dc032a
Klee Collage <klee.collage@gmail.com>
2025-04-20 16:00:59 -0600

    feat(pages): Implement estate/list page with all states from DB

--- src/app/pages/static/pages/not-found/not-found.component.ts
+++ src/app/pages/static/pages/not-found/not-found.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotFoundComponent implements OnInit {
 
+  image404: string = "https://firebasestorage.googleapis.com/v0/b/estates-app-d314b.firebasestorage.app/o/image%2F1745180065960_elite5.jpg?alt=media&token=b1a40c93-353d-4e1b-ab22-f4afde75581c";
   constructor() { }
 
   ngOnInit(): void {
